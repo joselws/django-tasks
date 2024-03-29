8f1b819c17adfe4bf7cd1e31cd9763203d80d255
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-03-29 19:42:38 -0400

    removed static files and admin site

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls.static import static
-# from django.conf import settings
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("tasks.urls")),]
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("", include("tasks.urls")),
+]
