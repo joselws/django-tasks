ea922dac6ba61e620d9cdea6462f47c915cdec9e
Jose Wilhelm <josel.wilhelms@gmail.com>
2024-03-29 18:10:45 -0400

    added build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+cd todolist
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# set up database
+python manage.py makemigrations
+python manage.py migrate
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
